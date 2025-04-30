18db83bbadbd2273fa5252ba3fa05a39ec8b2bed
HAMA25 <wlgns379@gmail.com>
2025-05-01 03:26:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const imageGrid = document.getElementById('image-grid');
-    const totalImages = 472;
 
-    // 이미지 동적 추가
-    for (let i = 1; i <= totalImages; i++) {
-        const img = document.createElement('img');
-        img.src = `images/${i}.jpg`; // 이미지 경로 설정
-        img.alt = `Image ${i}`;
-        imageGrid.appendChild(img);
-    }
-});
